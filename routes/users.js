564a7e1f4ebdaa2221624662f106cd46bd16d5b6
岑安琪 <ccccaqqqq@163.com>
2019-03-14 17:43:59 +0800

    哈哈

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
+
+router.use(function(req, res, next){
+  console.log(moment(new Date()).format('YYYY-MM-DD HH:mm'));
+  next();
+})
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-router.get('/cool', function(req, res, next){
-  res.send('You look so cool')
+router.post('/cool', function(req, res, next){
+  res.json({
+    message: `${req.body.name} look so cool`
+  })
 })
 
 module.exports = router;
